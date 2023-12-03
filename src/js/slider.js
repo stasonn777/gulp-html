e9c150e3661bc64f863208b8b13c7d7e7bb4c57f
StasLakhman <stasonn777@gmail.com>
2023-12-03 14:00:21 +0200

    clone slides

--- src/js/slider.js
+++ src/js/slider.js
@@ -20,33 +20,51 @@ class Slider {
     this.sliderWrapper = document.getElementById(this.id);
     this.controlLeft = this.sliderControl.children[0];
     this.controlRight = this.sliderControl.children[1];
+
+    [...this.sliderWrapper.children].forEach((el, i, arr) => {
+      const clone = el.cloneNode(true);
+      clone.classList.remove('active');
+       if (i <= arr.length-1 && i > 0) {
+        this.sliderWrapper.insertBefore(clone, this.sliderWrapper.children[i-1]);
+      } else {
+        return;
+      }
+    });
+
     this.slidesArr = [...this.sliderWrapper.children];
     this.slideWidth = this.slidesArr[0].clientWidth;
     this.sliderWidth = this.slidesArr[0].clientWidth * this.slidesArr.length;
     this.sliderWrapper.style.width = this.sliderWidth + 'px';
 
-    
     this.eventListenners();
     this.setLeftForSlides();
     this.initActive();
     this.doStatusBar();
     // debugger;
   }
-  
+
   eventListenners() {
     //Control buttons click event
-    this.sliderControl.addEventListener('click', (e) => this.doControl(e.target.dataset.control));
+    this.sliderControl.addEventListener('click', (e) =>
+      this.doControl(e.target.dataset.control)
+    );
 
     //Swipe events
     let touchStart = 0;
     let touchEnd = 0;
-    this.sliderWrapper.addEventListener('touchstart', (e) => touchStart = e.touches[0].clientX);
-    this.sliderWrapper.addEventListener('touchmove', (e) => touchEnd = e.touches[0].clientX);
+    this.sliderWrapper.addEventListener(
+      'touchstart',
+      (e) => (touchStart = e.touches[0].clientX)
+    );
+    this.sliderWrapper.addEventListener(
+      'touchmove',
+      (e) => (touchEnd = e.touches[0].clientX)
+    );
     this.sliderWrapper.addEventListener('touchend', (e) => {
       if (touchStart < touchEnd) {
-        this.doControl('left')
+        this.doControl('left');
       } else {
-        this.doControl('right')
+        this.doControl('right');
       }
     });
   }
@@ -55,17 +73,6 @@ class Slider {
     this.slidesArr.forEach((slide, i) => {
       slide.style.left = i * this.slideWidth + 'px';
     });
-  }
-
-  prevSlide() {
-    const last = this.slidesArr.pop(0);
-    this.slidesArr.unshift(last);
-    this.setLeftForSlides();
-    last.style.display = 'none';
-    setTimeout(() => {
-      last.style.display = 'block';
-    }, 0);
-    this.setActive(false);
   }
 
   doControl(e) {
@@ -77,6 +84,17 @@ class Slider {
         this.nextSlide();
         break;
     }
+  }
+
+  prevSlide() {
+    const last = this.slidesArr.pop(0);
+    this.slidesArr.unshift(last);
+    this.setLeftForSlides();
+    last.style.display = 'none';
+    setTimeout(() => {
+      last.style.display = 'block';
+    }, 0);
+    this.setActive(false);
   }
 
   nextSlide() {
@@ -118,7 +136,9 @@ class Slider {
       if (this.statusBullets[this.activeBullet.index - 1]) {
         this.statusBullets[this.activeBullet.index - 1].classList.add('active');
       } else {
-        this.statusBullets[this.statusBullets.length -1].classList.add('active');
+        this.statusBullets[this.statusBullets.length - 1].classList.add(
+          'active'
+        );
       }
     }
     // debugger
