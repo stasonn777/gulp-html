2ad37495c7df9fb933c5d11cd7c9dc0359c91c0c
StasLakhman <stasonn777@gmail.com>
2024-06-14 12:02:48 +0300

    Card and payment

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
-console.log('Hello gulp');
+const selectBank = document.querySelector('.select-bank')
+const bankOptions = document.querySelector('.bank-options')
+
+selectBank.addEventListener('click', () => {
+  bankOptions.classList.toggle("hide")
+})
+
